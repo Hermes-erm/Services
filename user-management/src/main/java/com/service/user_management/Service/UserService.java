c3ca854a7fa8f7fe88350e74d75ab35a619a2512
Hermes <karan1raider@gmail.com>
2025-06-09 10:57:13 +0530

    Update UserService.java

--- user-management/src/main/java/com/service/user_management/Service/UserService.java
+++ user-management/src/main/java/com/service/user_management/Service/UserService.java
@@ -3,13 +3,21 @@ package com.service.user_management.Service;
 import com.service.user_management.Repository.UserModel;
 import com.service.user_management.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+//@Data
 public class UserService {
     @Autowired
     private UserRepository repository;
+
+    private final MongoTemplate mongoTemplate;
+
+    UserService(MongoTemplate mongoTemplate) { // or u can use lombok, with ArgsConstructor!
+        this.mongoTemplate = mongoTemplate;
+    }
 
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
@@ -23,6 +31,8 @@ public class UserService {
     }
 
     public UserModel addNewUser(UserModel user) {
+        this.mongoTemplate.findOne();
+
         user.setPassword(this.encodePassword(user.getPassword()));
         UserModel updatedUser = this.repository.save(user);
         updatedUser.setPassword(null);
